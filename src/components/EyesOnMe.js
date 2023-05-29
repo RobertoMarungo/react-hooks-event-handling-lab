064de71ec135e6fc869a658e0b3953fcd03f1e7d
Roberto Marungo <100833599+RobertoMarungo@users.noreply.github.com>
2023-05-29 00:43:57 +0000

    Assignment Complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+
+    function handelFocus() {
+        console.log("Good!")
+    }
+
+    function handelBlur(){
+        console.log("Hey! Eyes on me!")
+    }
+    return(
+        <button onFocus={handelFocus} onBlur={handelBlur}>Eyes on me</button>
+    )
+}
+
+
+export default EyesOnMe;
